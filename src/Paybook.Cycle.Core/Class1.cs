b404068298a4ac1f0bfb82939b885bcf0559810c
Nelson Da Silva Neto <nelson.neto@stone.com.br>
2024-02-06 13:59:13 -0300

    teste integracao basico

---
+++ src/Paybook.Cycle.Core/Class1.cs
@@ -0,0 +1,27 @@
+﻿namespace Paybook.Cycle.Core
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+
+    public class Pagamento
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+    }
+
+    public interface IPagamentoRepository
+    { }
+
+    public class PagamentoRepository : IPagamentoRepository
+    { }
+}
